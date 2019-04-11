709d48457f78e6c063a685d23c9dbeaa0d659d12
Roland0624 <48576613+Roland0624@users.noreply.github.com>
2019-04-11 12:59:43 +0800

    Add files via upload

---
+++ HW11.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+	int a,b,c,tmp;
+	printf("½Ð¿é¤Ja,b,c");
+	scanf("%d%d%d",&a,&b,&c);
+	int array[3]={a,b,c};
+	for(int x=0;x<2;x++)
+		for(int n=x+1;n<3;n++)
+		{
+			if(array[n]>array[x])
+			{
+				tmp=array[x];
+				array[x]=array[n];
+				array[n]=tmp;
+			}
+		}
+		
+	for(int x=2;x>=0;x--){
+		printf("%d",array[x]);
+		printf(" ");
+	}
+}
